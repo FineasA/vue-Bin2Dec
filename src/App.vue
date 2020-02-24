f666bf903e9663780dab5949c8334ebac70dc102
Fineas Anton <fineas.anton@gmail.com>
2020-02-24 16:05:29 -0600

    binary to decimal converter with number animation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <h3 class="col">Bin2Dec Converter</h3>
+      <input class="col" type="text" v-model="inputNum" />
+      <h3 class="col">
+        <animated-number
+          :value="parseInt(Number(inputNum), 2)"
+          :duration="900"
+        ></animated-number>
+      </h3>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AnimatedNumber from "animated-number-vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld
+    AnimatedNumber
+  },
+  data() {
+    return {
+      inputNum: ""
+    };
+  },
+  computed: {
+    binaryToDecimal() {
+      return parseInt(this.inputNum, 2).toFixed(2);
+    }
+  },
+  methods: {
+    getEndStr(string) {
+      let end = string
+        .split("")
+        .splice(string.length - 1, string.length)
+        .join("");
+      if (end === "0" || end === "1") {
+        return;
+      } else {
+        this.deleteUnwanted(string);
+      }
+    },
+    deleteUnwanted(string) {
+      let newString = string
+        .split("")
+        .slice(0, string.length - 1)
+        .join("");
+      this.inputNum = newString;
+    },
+    checkStringLen(string) {
+      if (string.length > 8) {
+        let newStr = string
+          .split("")
+          .slice(0, -1)
+          .join("");
+        this.inputNum = newStr;
+      } else {
+        return;
+      }
+    }
+  },
+  updated() {
+    console.log(this.inputNum);
+    this.getEndStr(this.inputNum);
+    this.checkStringLen(this.inputNum);
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  min-height: 100%;
+  min-height: 100vh;
+
+  display: flex;
+  align-items: center;
 }
 </style>
